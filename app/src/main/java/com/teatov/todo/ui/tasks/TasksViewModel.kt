7705137fa9e3cf911c35b7d607c89049286eee0b
titov.aale <titov.aale@dvfu.ru>
2023-01-06 13:38:47 +1000

    Create menu

--- app/src/main/java/com/teatov/todo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/teatov/todo/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package com.teatov.todo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.teatov.todo.data.TaskDao
 
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
-) : ViewModel()
+) : ViewModel() {
+    val tasks = taskDao.getTasks().asLiveData()
+}
