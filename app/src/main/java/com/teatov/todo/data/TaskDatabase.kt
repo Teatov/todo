c0b19ed479be9589fddca41d7bd5431bbb6582b9
titov.aale <titov.aale@dvfu.ru>
2023-01-06 12:43:07 +1000

    Create database dependency injection and application coroutine scope

--- app/src/main/java/com/teatov/todo/data/TaskDatabase.kt
+++ app/src/main/java/com/teatov/todo/data/TaskDatabase.kt
@@ -2,10 +2,37 @@ package com.teatov.todo.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.teatov.todo.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
 
 @Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val dao = database.get().taskDao()
+
+            applicationScope.launch {
+                dao.insert(Task("wash the dishes"))
+                dao.insert(Task("kill"))
+                dao.insert(Task("live", important = true))
+                dao.insert(Task("buy bread", completed = true))
+                dao.insert(Task("eat veggies", completed = true))
+            }
+        }
+
+    }
 
 }
